58efc21691888fbc46a60f354e50641d737ffe45
kakarotto09 <ocampo_orlando@hotmail.com>
2023-05-29 21:56:30 -0300

    Desafio de Alura listo

---
+++ script.js
@@ -0,0 +1,81 @@
+const textArea = document.querySelector(".text-area"); // declaracion de variables almacena lo que el usuario va estar escribiedo en el text area
+const mensaje = document.querySelector(".mensaje"); //declaracion de variable donde va a mostrar texto encriptado
+
+//llaves de encriptacion
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+// funcion copiar //
+function copiarTexto(){
+
+  // Selecciona el elemento que contiene el texto a copiar
+  const copiarTexto= document.querySelector(".mensaje");
+  copiarTexto.select();
+  document.execCommand("copy");
+  alert("¡Texto copiado al portapapeles!");
+}
+
+// funcion del boton encriptar
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
+}
+
+// encriptando//
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      ); // remplazo de la vocales, VERIFICACION DEL ARRAY SE COLOCA UNA FUNCION
+    }
+  }
+  return stringEncriptada;
+}
+
+// funcion del boton desencriptar
+function btnDesencriptar() {
+  const textoEncriptado = desencriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+}
+
+// funcion desencriptar //
+
+function desencriptar(stringDesencriptado) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptado = stringDesencriptado.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+      stringDesencriptado = stringDesencriptado.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      ); // remplazo de la vocales, VERIFICACION DEL ARRAY SE COLOCA UNA FUNCION
+    }
+  }
+  return stringDesencriptado;
+}
+
